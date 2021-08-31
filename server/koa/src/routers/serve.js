e18c15e9afc2ce948bb2e4f057beb1cf06e94258
黄晓林 <wx0huangxl@inspur.com>
2021-08-31 20:04:50 +0800

    增加huboan的接口路由

--- server/koa/src/routers/serve.js
+++ server/koa/src/routers/serve.js
@@ -2,7 +2,7 @@
  * @Author       : xiaolin
  * @Date         : 2021-08-31 10:09:44
  * @LastEditors  : xiaolin
- * @LastEditTime : 2021-08-31 10:17:53
+ * @LastEditTime : 2021-08-31 19:55:56
  * @Description  : 后台路由
  * @FilePath     : \lotteryMeanage\server\koa\src\routers\serve.js
  */
@@ -24,10 +24,12 @@ const serveController = require("../controllers/serveController");
 router.post("/login", serveController.login);
 
 // 文档 => 2.2 基础设置
-router.post("/baseConfig", serveController.baseConfig);
+router.get("/getOreInit", serveController.getOreInit);
+router.get("/getOreUse", serveController.getOreUse);
 
 // 文档 => 2.3 修改基础设置
-router.post("/setBaseConfig", serveController.setBaseConfig);
+router.post("/setOreInit", serveController.setOreInit);
+router.post("/setOreUse", serveController.setOreUse);
 
 // 文档 => 2.4 读取抽奖纪录
 router.get("/list", serveController.listAllRecord);
@@ -35,7 +37,7 @@ router.get("/list", serveController.listAllRecord);
 router.post("/delete", serveController.deleteRecord);
 
 // 文档 => 2.5 读取发货纪录
-router.post("/transport", serveController.transport);
+router.get("/transport", serveController.transport);
 
 // 文档 => 2.6 修改发货纪录
 router.post("/setTransport", serveController.setTransport);
