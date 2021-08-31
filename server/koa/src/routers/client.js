3c211d74552bd7270436efb7f3fb226d6a86b170
黄晓林 <wx0huangxl@inspur.com>
2021-08-31 23:12:11 +0800

    补充 1.8文档

--- server/koa/src/routers/client.js
+++ server/koa/src/routers/client.js
@@ -2,7 +2,7 @@
  * @Author       : xiaolin
  * @Date         : 2021-08-31 09:28:58
  * @LastEditors  : xiaolin
- * @LastEditTime : 2021-08-31 23:05:21
+ * @LastEditTime : 2021-08-31 23:11:53
  * @Description  : 前台接口
  * @FilePath     : \lotteryMeanage\server\koa\src\routers\client.js
  */
@@ -38,7 +38,7 @@ router.get("/lottery", clientController.lottery);
 // 文档 => 1.7 收获信息
 router.get("/address", clientController.address);
 
-//
+// 文档 => 1.8 创建用户
 router.get("/create", clientController.create);
 
 module.exports = router.routes();
